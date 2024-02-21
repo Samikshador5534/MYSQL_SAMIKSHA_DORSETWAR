5a4476398c3a214a44744135cd7f9f8e23abe99c
Samikshador5534 <paidlewarsamiksha@gmail.com>
2024-02-21 11:33:48 +0530

    21 feb, mysql file added

---
+++ Create_Table_Mkpits.sql
@@ -0,0 +1,4 @@
+create table MKPITS( StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(50),
+    LastName VARCHAR(50),
+    City VARCHAR(50));
---
+++ Inser_Dtabase_Values.sql
@@ -0,0 +1,4 @@
+insert into MKPITS values(2, 'Vaibhav' , 'Paidlewar','pune');
+insert into MKPITS values(3, 'Harsh' , 'Gupta','Nagpur');
+insert into MKPITS values(4, 'Raj' , 'Warorkar','kamptee');
+insert into MKPITS values(1, 'Samiksha' , 'Dorsetwar','Wardha');
